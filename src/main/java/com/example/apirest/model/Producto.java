34d687130008e1a761e51ff7989d4a4d531ecd9c
Gutavo Martinez <gutamvol262@gmail.com>
2023-07-19 14:03:17 -0600

    Hacer las entidades a utilizar

---
+++ src/main/java/com/example/apirest/model/Producto.java
@@ -0,0 +1,21 @@
+package com.example.apirest.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Producto {
+
+
+    private int id;
+    private String nombre;
+    private  int cantidad;
+    private  double precio;
+
+
+
+}
